2a6c7248536b86afe747828ee262cdb7610c9260
RL-008 <rl-008@charlene.local>
2017-10-23 16:09:15 +0800

    Removed unneccessary variables

--- sub/square.py
+++ sub/square.py
@@ -11,12 +11,11 @@ class Square(object):
      h = heuristic value
      g = path cost from the starting point
      f = h + g
-     is_path = will be set true if and only if the square is
-               part of the path
      parent = the parent square of this square
     '''
     def __repr__(self):
         return str((str(self.x), str(self.y))) + " h = " + str(self.h) 
+
     def __init__(self, x=0, y=0, s_type=0):
         self.x = x
         self.y = y
@@ -24,16 +23,4 @@ class Square(object):
         self.h = 0
         self.g = 0
         self.f = 0
-        self.is_path = False
         self.parent = None
-
-
-    def set_h(self, h=0):
-        if self.h != 0 and h < self.h:
-            self.h = h
-            self.f = self.g + self.h
-
-    def set_g(self, g=0):
-        if self.g != 0 and g < self.g:
-            self.g = g
-            self.f = self.g + self.h
